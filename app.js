f35d352f32ef634562ac2a62e56ac99162d3c0df
Fig <bethanytrr@gmail.com>
2023-10-04 12:35:02 -0400

    Updated for legibility.

--- app.js
+++ app.js
@@ -2,38 +2,14 @@ const express = require("express")
 
 const app = express()
 const PORT = 3003
-const magic8Responses = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes - Definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes, and signs point to yes",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful" 
-  ]
 
-
+//HELLO WORLD
 
 app.get('/', (req,res) => {
     res.send("Hewwo Worwd! owo")
 })
 
-app.get('/magic8ball', (req,res) => {
-    const magic8Answer = magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
-    res.send(`<h1>${magic8Answer}</h1>`)
-})
+//CHARACTERS AND CATCHPHRASES
 
 app.get('/terminator', (req, res) =>{
     res.send("I'll be back!")
@@ -82,6 +58,37 @@ app.get('/brucebanner', (req, res) =>{
 app.get('/jimlovell', (req, res) =>{
     res.send("Houston, we have a problem")
 })
+
+
+// MAGIC 8 BALL
+
+app.get('/magic8', (req,res) => {
+    const magic8Responses = [
+        "It is certain!",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - Definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes, and signs point to yes.",
+        "Reply hazy, try again...",
+        "Ask again later...",
+        "Better not tell you now...",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful." 
+      ]    
+    const magic8Answer = magic8Responses[Math.floor(Math.random() * magic8Responses.length)];
+    res.send(`<h1>${magic8Answer}</h1>`)
+})
+
+//APP LISTENER
 
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}.`)
