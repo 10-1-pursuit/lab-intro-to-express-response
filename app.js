1ff252cd6bd1587b95386738851374b6cc7ff613
xavierrice <xavier.rice@gmail.com>
2023-10-04 10:40:50 -0400

    working on magic8 and tests

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import getRandomAnswer from "./magic8Response";
+const getRandomAnswer = require( "./magic8Response");
 const express = require("express") 
 const app = express(); 
 
@@ -16,31 +16,32 @@ app.get("/tim-gunn", (req, res) => {
     res.send("Make it Work")
 });
 
-app.get("/RuPaul", (req, res) => {
+app.get("/rupaul", (req, res) => {
     res.send("Sasshay--you stay")
     res.send("You betta Work!") // each request can only have 1 response so it never gets to this!
 });
 
-app.get("/Emeril", (req, res) => {
+app.get("/emeril", (req, res) => {
     res.send("Bam!!!!")
 });
 
-app.get("/Borg", (req, res) => {
+app.get("/borg", (req, res) => {
     res.send("Resistance is Futile")
 });
 
-app.get("/Zeus", (req, res) => {
+app.get("/zeus", (req, res) => {
     res.send("Who's Unicorn is This!?")
 });
 
-app.get("/Toto", (req, res) => {
+app.get("/toto", (req, res) => {
     res.send("Gimme my shoes Back!!")
 });
 
-app.get("/Gollum", (req, res) => {
+app.get("/gollum", (req, res) => {
     res.send("My Precious!")
 });
 
 app.get("/magic8", (req, res) => {
-    res.send(`<h1>${getRandomAnswer}<h1>`)
-})
+    const randomAnswer = getRandomAnswer()
+    res.render(`<h1>${randomAnswer}</h1>`)
+});
