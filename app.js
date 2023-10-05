6dbc7aaa9a52e570cf2532152a8041543fc95ded
ShakaPursuit <shakalivngstone@pursuit.org>
2023-10-04 23:22:14 -0400

    finished 8ball

--- app.js
+++ app.js
@@ -43,4 +43,36 @@ app.get('/Emeril', function (req, res) {
     res.send('I\m listening')
   })
 
+
+  const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  
+  ]
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  app.get('/8Ball', function (req, res) {
+    res.send(`<h1>${magic8Responses[getRandomInt(19)]}</h1>`)
+  })
+
 app.listen(3000)
