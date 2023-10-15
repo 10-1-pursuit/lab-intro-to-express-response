a58adb691ebdc75ce434d6f055a678b41b8d9210
Kenneth-Parker <kennethkavon@gmail.com>
2023-10-15 18:21:28 -0400

    status 200

--- app.js
+++ app.js
@@ -4,57 +4,56 @@ const express = require("express")
 // Configuration
 const app = express()
 
-
 // Routes
 app.get("/", (request, response) => {
-    response.send("What's Poppin Ken?");
-  });
+    response.status(200).send("What's Poppin Ken?");
+});
 
 app.get("/terminator", (request, response) => {
-    response.send("I'll be back");
-  });
+    response.status(200).send("I'll be back");
+});
 
 app.get('/tim-gunn', (req, res) => {
-    res.send(`<center>
+    res.status(200).send(`<center>
     <h1>Make it work</h1>
-  </center>`)
-  })
+  </center>`);
+});
 
 app.get('/Emeril', (req, res) => {
-    res.send(`<center><h1>Bam!</h1></center>`)
-})
+    res.status(200).send(`<center><h1>Bam!</h1></center>`);
+});
 
 app.get('/Steve%20McGarrett', (req, res) => {
-    res.send(`<center><h1>Book 'em Danno!</h1></center>`)
-})
+    res.status(200).send(`<center><h1>Book 'em Danno!</h1></center>`);
+});
 
 app.get('/Coach-Taylor', (req, res) => {
-    res.send(`<center><h1>Clear eyes, full hearts, can't Lose</h1></center>`)
-})
+    res.status(200).send(`<center><h1>Clear eyes, full hearts, can't Lose</h1></center>`);
+});
 
 app.get('/Homer-Simpson', (req, res) => {
-    res.send(`<center><h1>D'Oh</h1></center>`)
-})
+    res.status(200).send(`<center><h1>D'Oh</h1></center>`);
+});
 
 app.get('/Bruce-Banner', (req, res) => {
-    res.send(`<center><h1>Dont't make me angry</h1></center>`)
-})
+    res.status(200).send(`<center><h1>Dont't make me angry</h1></center>`);
+});
 
 app.get('/JJ-Evans', (req, res) => {
-    res.send(`<center><h1>Dy-no-myte!</h1></center>`)
-})
+    res.status(200).send(`<center><h1>Dy-no-myte!</h1></center>`);
+});
 
 app.get('/Batman', (req, res) => {
-    res.send(`<center><h1>To the Batmobile</h1></center>`)
-})
+    res.status(200).send(`<center><h1>To the Batmobile</h1></center>`);
+});
 
 app.get('/Hannibal-Smith', (req, res) => {
-    res.send(`<center><h1>I love it when a plan comes together</h1></center>`)
-})
+    res.status(200).send(`<center><h1>I love it when a plan comes together</h1></center>`);
+});
 
 app.get('/Travis-Bickle', (req, res) => {
-    res.send(`<center><h1>You talkin' to me</h1></center>`)
-})
+    res.status(200).send(`<center><h1>You talkin' to me</h1></center>`);
+});
 
 const magic8Responses = [
     "It is certain",
@@ -82,10 +81,8 @@ const magic8Responses = [
     const randomIndex = Math.floor(Math.random() * magic8Responses.length);
     const randomResponse = magic8Responses[randomIndex];
     
-    res.send(`<center><h1 style="color: red;">${randomResponse}</h1></center>`);
-
+    res.status(200).send(`<center><h1 style="color: red;">${randomResponse}</h1></center>`);
   });
-
 
 // Export the app object
 module.exports = app;
