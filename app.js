aaade15fd38320230bb7bcd9d9bccd3ed2f17c43
Yulonda <yulondawyatt@pursuit.org>
2023-10-19 12:21:04 -0400

    Fixed my app.get problem. Needed to add the foward slash before the catchphrase

--- app.js
+++ app.js
@@ -7,43 +7,43 @@ const PORT = 3003
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello my Fellow\s in Cohort 10.1!!')
+    res.send('Hello my Fellows in Cohort 10.1!!')
 })
 
-app.get('terminator', (req, res) => {
-    res.send('1. I\ll be back!')
+app.get('/terminator', (req, res) => {
+    res.send('I\'ll be back!')
 })
 
-app.get('bruce-willis', (req, res) => {
-    res.send('2. Yippee Ki Yay!')
+app.get('/bruce-willis', (req, res) => {
+    res.send('Yippee Ki Yay!')
 })
 
-app.get('homer-simpson', (req, res) => {
-    res.send('3. D\Oh!')
+app.get('/homer-simpson', (req, res) => {
+    res.send('D\'Oh!')
 })
 
-app.get('borg', (req, res) => {
-    res.send('4. Resistance is futile!')
+app.get('/borg', (req, res) => {
+    res.send('Resistance is futile!')
 })
 
-app.get('dorothy', (req, res) => {
-    res.send('5. Toto, I\ve got a feeling we\\re not in Kansas anymore!')
+app.get('/dorothy', (req, res) => {
+    res.send('Toto, I\'ve got a feeling we\'re not in Kansas anymore!')
 })
 
-app.get('dean-winchester', (req, res) => {
-    res.send('6. Awesome!')
+app.get('/dean-winchester', (req, res) => {
+    res.send('Awesome!')
 })
 
-app.get('gene-kelly', (req, res) => {
-    res.send('7. I\m singing in the rain!')
+app.get('/gene-kelly', (req, res) => {
+    res.send('I\'m singing in the rain!')
 })
 
-app.get('pharrell-williams', (req, res) => {
-    res.send('8. So Happy!')
+app.get('/pharrell-williams', (req, res) => {
+    res.send('Because I\'m Happy!')
 })
 
-app.get('gloria-gaynor', (req, res) => {
-    res.send('9. I will survive!')
+app.get('/gloria-gaynor', (req, res) => {
+    res.send('I will survive!')
 })
 
 // LISTEN
